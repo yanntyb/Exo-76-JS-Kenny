cb7815d4043726d17946b89aa4797422e8561a67
yanntyb <69966391+yanntyb@users.noreply.github.com>
2020-12-04 11:32:53 +0100

    a

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ document.getElementById('up').addEventListener('click',function() {
     t = t - 10;
     document.getElementById('kenny').style.top = t + 'px';
 });
+
+document.getElementById('down').addEventListener('click',function() {
+    let t = parseInt(document.getElementById('kenny').style.top);
+    t = t + 10;
+    document.getElementById('kenny').style.top = t + 'px';
+});
+
+document.getElementById('left').addEventListener('click',function() {
+    let l = parseInt(document.getElementById('kenny').style.left);
+    l = l - 10;
+    document.getElementById('kenny').style.left = l + 'px';
+});
+
+document.getElementById('right').addEventListener('click',function() {
+    let r = parseInt(document.getElementById('kenny').style.top);
+    r = r + 10;
+    document.getElementById('kenny').style.left = r + 'px';
+});
